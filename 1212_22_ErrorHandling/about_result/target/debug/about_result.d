71fcf17169f1b82d7dc64759c8d0b576a2e7d8f8
sailaoda <1520516332@qq.com>
2022-12-12 10:30:42 +0800

    【特性】：panic and result panic

---
+++ 1212_22_ErrorHandling/about_result/target/debug/about_result.d
@@ -0,0 +1,1 @@
+E:\desktopFiles\projects\rust\1212_22_ErrorHandling\about_result\target\debug\about_result.exe: E:\desktopFiles\projects\rust\1212_22_ErrorHandling\about_result\src\main.rs
---
+++ 1212_22_ErrorHandling/about_result/target/debug/deps/about_result.d
@@ -0,0 +1,5 @@
+E:\desktopFiles\projects\rust\1212_22_ErrorHandling\about_result\target\debug\deps\about_result.exe: src\main.rs
+
+E:\desktopFiles\projects\rust\1212_22_ErrorHandling\about_result\target\debug\deps\about_result.d: src\main.rs
+
+src\main.rs:
