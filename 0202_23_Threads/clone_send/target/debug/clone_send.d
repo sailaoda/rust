4bd022a4013cd3ab222115466411c2010b614eaa
sailaoda <1520516332@qq.com>
2023-02-02 16:25:09 +0800

    【特性】：clone send

---
+++ 0202_23_Threads/clone_send/target/debug/clone_send.d
@@ -0,0 +1,1 @@
+E:\desktopFiles\projects\rust\0202_23_Threads\clone_send\target\debug\clone_send.exe: E:\desktopFiles\projects\rust\0202_23_Threads\clone_send\src\main.rs
---
+++ 0202_23_Threads/clone_send/target/debug/deps/clone_send.d
@@ -0,0 +1,5 @@
+E:\desktopFiles\projects\rust\0202_23_Threads\clone_send\target\debug\deps\clone_send.exe: src\main.rs
+
+E:\desktopFiles\projects\rust\0202_23_Threads\clone_send\target\debug\deps\clone_send.d: src\main.rs
+
+src\main.rs:
